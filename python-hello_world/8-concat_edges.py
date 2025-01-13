62d9894c04f1179fb599633228c26ebb4207f4ea
MINS2405 <9963@holbertonstudents.com>
2025-01-13 21:09:55 +0100

    Python - Hello, World

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-str1 = "object-oriented programming"
-str2 = "with Python"
-print(f"{str1}")
-print(f"{str2}")
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
+
 
 
