cfd7657b82883d0dd8988078f653876d98b1efdb
MINS2405 <9963@holbertonstudents.com>
2025-01-13 21:15:43 +0100

    Python - Hello, World

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
-number = 98
-
-print(f"{98} Battery street")
+number = number
+print(f"{number} Battery street")
--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(f"{)
+print(str * 3)
+print(str[:9])
