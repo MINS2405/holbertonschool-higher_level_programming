9dd278ebbb010a3c11fa70629d1baea2a965b6af
MINS2405 <9963@holbertonstudents.com>
2025-01-15 13:46:59 +0000

    Python - import & modules

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    import sys
-
-    result = sum(int(arg) for arg in sys.argv[1:])
-    print(result)
-
+    total = 0
+    for arg in sys.argv[1:]:
+        try:
+            total += int(arg)
+        except ValueError:
+            print(f"Erreur : '{arg}' n'est pas un nombre valide.")
+            sys.exit(1)
+    print(total)
