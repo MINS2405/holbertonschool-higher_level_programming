2b0a20d6cfdf85865f0df8286d6b314b8fac338e
MINS2405 <9963@holbertonstudents.com>
2025-01-26 19:01:25 +0000

    Python - Test-driven development

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,20 +2,17 @@
 def say_my_name(first_name, last_name=""):
     """
     Imprime "My name is <first name> <last name>"
-
     Args:
-    first_name (str): Le prénom
-    last_name (str, optional): Le nom de famille. Par défaut "".
-
+    first_name (str): Le préno
+    last_name (str, optional): Le nom de famille. Par défaut ""
     Raises:
-    TypeError: Si first_name ou last_name n'est pas une chaîne de caractères.
-
+    TypeError: Si first_name ou last_name n'est pas une chaîne de caractèr.
     >>> say_my_name("John", "Smith")
     My name is John Smith
     >>> say_my_name("Walter", "White")
     My name is Walter White
     >>> say_my_name("Bob")
-    My name is Bob 
+    My name is Bob
     >>> say_my_name(12, "White")
     Traceback (most recent call last):
         ...
@@ -25,5 +22,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
     print(f"My name is {first_name} {last_name}")
