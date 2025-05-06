8fe012077918372bc58e8f19198d610036408b2a
MINS2405 <9963@holbertonstudents.com>
2025-05-06 12:19:42 +0200

    change file 5-print_string.py

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(f"{str} {str} {str}")
-print(f"{str} {str} {str}")
-print(f"{str} {str} {str}")
-print(f"{str} {str} {str}")
-print(f"{str} {str} {str}")
-print(f"{str} {str} {str}")
-print(f"{str} {str} {str}")
+print(str * 3)
+print(str[:9])
