13814a89b3451c1431672d0d4df0a63417723559
MINS2405 <9963@holbertonstudents.com>
2025-05-06 12:24:46 +0200

    change file 6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -2,8 +2,4 @@
 str1 = "Holberton"
 str2 = "School"
 str3 = str1 + ' ' + str2
-print(f"Welcome to {str3}!")
-# The f-string format is used to concatenate the two strings with a space in between.
-# The print function is used to print the concatenated string.
-# The output will be:
-# Welcome to Holberton School!
+print(f"Welcome to {str1}!")
