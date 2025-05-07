809f972bd2a08681d6f562f1a28ab5ee5d584d5a
MINS2405 <9963@holbertonstudents.com>
2025-05-07 12:52:06 +0200

    change file 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
+    """Function to convert a string to uppercase"""
     for c in str:
         if ord('a') <= ord(c) <= ord('z'):
             print("{}".format(chr(ord(c) - 32)), end="")
