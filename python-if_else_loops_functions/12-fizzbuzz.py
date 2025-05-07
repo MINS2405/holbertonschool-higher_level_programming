9bfb42961ca430dab06c519ce336b73cd3b4e625
MINS2405 <9963@holbertonstudents.com>
2025-05-07 13:27:04 +0200

    file 12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    """Function to print numbers from 1 to 100, replacing multiples
-    of 3 with 'Fizz', multiples of 5 with 'Buzz', and multiples of
-    both with 'FizzBuzz'"""
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
