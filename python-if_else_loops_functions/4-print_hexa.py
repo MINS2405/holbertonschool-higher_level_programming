c390fc536b1159cd28da81d33eec5decd9c327ce
MINS2405 <9963@holbertonstudents.com>
2025-05-07 11:52:37 +0200

    change file 4-print_hexa.py

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-print("{}".format("".join([str(i) + " = " + hex(i)
-                           for i in range(0, 99)])), end="")
+print("\n".join("{} = {}".format(i, hex(i)) for i in range(99)))
