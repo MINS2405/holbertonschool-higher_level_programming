c63ad47055a7cca88e5913be5c01cbf29a4b2d64
MINS2405 <9963@holbertonstudents.com>
2025-05-12 09:17:04 +0200

    change file 0-print_list_integer.py

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-
-my_list = [1, 2, 3, 4, 5]
-print(f"number {1, 2, 3, 4, 5} !")
+    """Prints all integers of a list."""
+    for i in my_list:
+        print("{:d}".format(i))
+# Example usage:
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
