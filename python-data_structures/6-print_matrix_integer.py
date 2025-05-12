3a4173c83e371c1794f79d25125d1a94dba72fc6
MINS2405 <9963@holbertonstudents.com>
2025-05-12 10:20:15 +0200

    file 6-print_matrix_integer.py

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers."""
+    for row in matrix:
+        print(" ".join("{:d}".format(i) for i in row))
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3 [$]],
+        [4, 5, 6 [$]],
+        [7, 8, 9 [$]],
+        [-, -, - [$]],
+        ['$']
+    ]
+    print_matrix_integer(matrix)
