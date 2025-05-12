b2874099a3d141df128bb0731d84398c1e3c82fc
MINS2405 <9963@holbertonstudents.com>
2025-05-12 09:43:11 +0200

    file 1-element_at.py

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """Retrieves an element from a list like in C."""
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
+
+    if __name__ == "__main__":
+      my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    print("Element at index {}: {}".format(idx, element_at(my_list, idx)))
+    idx = 4
+    print("Element at index {}: {}".format(idx, element_at(my_list, idx)))
