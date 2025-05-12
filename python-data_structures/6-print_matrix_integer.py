28a479c8b24398e9e45574c7c8199fea98c214d6
MINS2405 <9963@holbertonstudents.com>
2025-05-12 10:22:06 +0200

    change file 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,13 +2,15 @@
 def print_matrix_integer(matrix=[[]]):
     """Prints a matrix of integers."""
     for row in matrix:
-        print(" ".join("{:d}".format(i) for i in row))
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end="")
+            if i < len(row) - 1:
+                print(" ", end="")
+        print()
 if __name__ == "__main__":
     matrix = [
-        [1, 2, 3 [$]],
-        [4, 5, 6 [$]],
-        [7, 8, 9 [$]],
-        [-, -, - [$]],
-        ['$']
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
     ]
     print_matrix_integer(matrix)
