da2da26de760388ad1b14c6ae6acfd68f9b6a053
MINS2405 <9963@holbertonstudents.com>
2025-05-12 09:52:18 +0200

    3-print_reversed_list_integer.py

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    """Prints all integers of a list, in reverse order."""
+    if my_list is not None:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
+    else:
+        print("None")
+
+if __name__ == "__main__":
+    my_list = [5, 4, 3, 2, 1]
+    print_reversed_list_integer(my_list)
+    my_list = None
+    print_reversed_list_integer(my_list)
