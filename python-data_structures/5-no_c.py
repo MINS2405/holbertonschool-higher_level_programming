2436eb03b8464fbfba1f45043243a22a09d09b71
MINS2405 <9963@holbertonstudents.com>
2025-05-12 10:09:33 +0200

    change file 5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,8 @@
 def no_c(my_string):
     """Removes all characters c and C from a string."""
     return my_string.translate({ord('c'): None, ord('C'): None})
+
+
 if __name__ == "__main__":
     my_string = "Best School"
     print(no_c(my_string))
