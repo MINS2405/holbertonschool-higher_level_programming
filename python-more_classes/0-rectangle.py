440b965c96698dde598d296ed51190447bd8020c
MINS2405 <9963@holbertonstudents.com>
2025-05-20 23:44:13 +0200

    change file 0-rectangle.py

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-"""define an empty class named Rectangle as Rectangle """
+"""
+This module defines an empty class named Rectangle.
+
+The Rectangle class is intended to represent a geometric rectangle.
+Currently, it does not have any attributes or methods.
+"""
 
 
 class Rectangle:
+    """An empty class that defines a rectangle."""
     pass
