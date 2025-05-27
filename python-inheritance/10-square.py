efbfeb1f9ef6db2b1ab79304b14a9cb7db4e00c1
MINS2405 <9963@holbertonstudents.com>
2025-05-27 12:31:04 +0200

    change file 10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+'''
+This module defines the Square class, which inherits from Rectangle.
+It validates and stores size, computes area, and provides a
+string representation.
+'''
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
