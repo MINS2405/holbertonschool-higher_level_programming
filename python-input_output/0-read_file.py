99fe1dc8501812f5d8d66fab99fd001b577b2c46
MINS2405 <9963@holbertonstudents.com>
2025-06-03 13:55:01 +0200

    change file 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+'''
+This script defines a function to read and print the content of
+a UTF-8 text file.
+'''
+
+
 def read_file(filename=""):
+    '''
+    Reads a UTF-8 text file and prints its content to stdout.
+    The filename parameter is the name of the file to read.
+    '''
+
     with open('my_file_0.txt', encoding="UTF-8") as f:
         print(f.read())
