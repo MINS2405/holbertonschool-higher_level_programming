b78669fb67ddd0c7cc34a808d386e34082ccf737
MINS2405 <9963@holbertonstudents.com>
2025-06-03 15:29:26 +0200

    change file 3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -18,12 +18,3 @@ def to_json_string(my_obj):
         A string containing the JSON representation of my_obj.
     '''
     return json.dumps(my_obj)
-
-
-print(to_json_string({
-    "id": 12,
-    "name": "John",
-    "places": ["San Francisco", "Tokyo"],
-    "is_active": True,
-    "info": {"age": 36, "average": 3.14}
-}))
