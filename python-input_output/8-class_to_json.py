e0603096cbbfe345cf045ef9b5fee47718cddb69
MINS2405 <9963@holbertonstudents.com>
2025-06-03 18:56:13 +0200

    change file 8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+'''
+This line tells the system to use Python 3 to run this script
+ '''
+
+
 def class_to_json(obj):
     return obj.__dict__
+
+
+'''
+Return the dictionary representation of the object's attributes
+'''
