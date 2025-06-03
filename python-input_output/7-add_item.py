40a36eea1b84b4088348a582a9963af32734991c
MINS2405 <9963@holbertonstudents.com>
2025-06-03 18:53:35 +0200

    change file 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ then saves them to a file in JSON format.
 '''
 
 import sys
-save_to_json_file = __import__ ('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__ ('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 try:
