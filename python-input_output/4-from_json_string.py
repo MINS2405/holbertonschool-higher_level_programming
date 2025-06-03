983eaeb17ac9ade0e6831e87658b5c9b71b487e6
MINS2405 <9963@holbertonstudents.com>
2025-06-03 15:52:14 +0200

    fil-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+This module provides a function to convert a JSON string
+into the corresponding Python data structure.
+'''
+
+import json
+
+def from_json_string(my_str):
+    '''
+    Returns the Python object represented by a JSON string.
+
+    Args:
+        my_str: The JSON string to convert.
+
+    Returns:
+        The corresponding Python object (dict, list, etc.).
+    '''
+    return json.loads(my_str)
