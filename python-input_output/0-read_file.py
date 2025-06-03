1883aafa566a3cd691e4d040f4ba69ec970fecda
MINS2405 <9963@holbertonstudents.com>
2025-06-03 13:46:09 +0200

    change file 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-with open('mon_fichier.txt', encoding="UTF-8") as f:
-    print(f.read())
+def read_file(filename=""):
+    with open('my_file_0.txt', encoding="UTF-8") as f:
+        print(f.read())
