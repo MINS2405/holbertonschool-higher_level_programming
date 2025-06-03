b8b90519bb4dda98d00b913b2936bfd3e82432f9
MINS2405 <9963@holbertonstudents.com>
2025-06-03 14:05:49 +0200

    1-main.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+This script defines a function to write text into a UTF-8 text file.
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    Writes the given text to a UTF-8 text file.
+    The function creates the file if it does not exist, or overwrites
+    it if it does.
+    Returns the number of characters written.
+    '''
+    with open(filename, mode="w", encoding="UTF-8") as f:
+        print(f.write(text))
