8fb3730f4260edc0b700165e0eddc6c893dfabfa
MINS2405 <9963@holbertonstudents.com>
2025-06-05 12:50:40 +0200

    change task_01_pickle.py

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
 import pickle
-class CustomObjet:
 
-def __init__(self, name, age, is_student):
-self.name = name
-self.age = age
-self.is_student = is_student
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
 
-def display(self):
-print("Name:", self.name)
-print("Age:", self.age)
-print("Is Student:", self.is_student)
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Is Student:", self.is_student)
 
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
+        except Exception:
+            pass
 
-mon_objet = {"name": "John", "age": 25, "is_student": True}
-
-with open(''my_file.pkl', 'wb') as file:
-pickle.dump(my_objet, file)
-with open('my_file.pkl', 'rb') as file:
-    objet_charge = pickle.load(file)
-
-print(objet_charge)
-
-def load_object_from_file(filename):
-    try:
-        with open(filename, 'rb') as file:
-            return pickle.load(file)
-    except (FileNotFoundError, pickle.UnpicklingError, EOFError, Exception):
-        return None
-obj = load_object_from_file('my_file.pkl')
-print(obj)
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as file:
+                return pickle.load(file)
+        except Exception:
+            return None
